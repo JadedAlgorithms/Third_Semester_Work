a1f46c7d38b3832c6c25ee35e858e054804be8de
JadedAlgorithms <algorfidelis247@gmail.com>
2025-08-13 08:15:43 +0530

    Added LabCycle4 6.c

--- DSA/LabCycle4/6.c
+++ DSA/LabCycle4/6.c
@@ -1,110 +1,89 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX 50
+int q[MAX];
+int front=-1,rear=-1;
 
-#define MAX 10
-
-int queue[MAX];
-int front = -1, rear = -1;
-
-void enqueue(int data){
-    if(rear == MAX-1){
-        printf("Queue overflow!\n");
-        return;
-    }
-    else
-    {
-        if (front == -1) {
-            front = 0;
-        }
-        rear+=1;
-        queue[rear] = data;
-        return;
-    }
+void enque(int data)
+{
+	if(rear == MAX-1)
+		printf("queue full");
+	else{
+		rear++;
+		q[rear]=data;
+	}
 }
 
-int dequeue(){
-    if(front == -1 || front > rear){
-        printf("Queue underflow!\n");
-        return 0;
-    }
-    else{
-        int data = queue[front];
-        front+=1;
-        return data;
-    }
+int deque()
+{
+	if( front == rear )
+		printf("empty");
+	else{
+		return q[++front];
+		}
 }
 
-int peek(){
-    if (front == -1 || front > rear){
-        printf("Queue empty\n");
-        return 0;
-    }
-    else{
-        return queue[front];
-    }
+void display()
+{
+	if(front == rear)
+		printf("empty");
+	else{
+		printf("Queue:\n");
+		for(int i=front+1;i<=rear;i++)
+		{	printf("%d  ",q[i]);}
+		printf("\n");
+		}
 }
 
-void display(){
-    if (front == -1 || front > rear){
-        printf("Empty Queue\n");
-    }
-    else{
-        for(int i = front; i<=rear;i++){
-            printf("%d\t", queue[i]);
-        }
-        printf("\n");
-    }
-}
-#define MAX 10
+//aux stack
+int S[MAX];
+int top=-1;
 
-int stack[MAX];
-int top =-1;
-
-int is_empty(){
-    return top ==-1;
+void push(int data)
+{
+	if(top==MAX-1) printf("OverFlow\n");
+	else
+	{
+	top++;
+	S[top]=data;
+	}
 }
 
-int is_full(){
-    return top == MAX-1;
+int pop()
+{
+	if(top==-1) printf("UnderFlow\n");
+	else
+	{
+	return S[--top];
+	}
 }
 
-void push(int data){
-    if (is_full()){
-        printf("Stack Overflow!\n");
-        return;
-    }
-    stack[++top] = data;
-    printf("Pushed %d \n",data);
+//reversing
+void reverse()
+{
+	while (front!=rear)
+	{
+		push(deque());
+	}
+	front=rear=-1;
+	
+	while(top!=0)
+	{
+		enque(pop());
+	}
 }
 
-int pop(){
-    if (is_empty()){
-        printf("Stack Underflow! \n");
-        exit(1);
-    }
-    printf("Popped  %d \n",stack[top]);
-    return stack[--top];
-}
-
-int peek(){
-    if (is_empty()){
-        printf("Stack Underflow!\n");
-        exit(1);}
-    return stack[top];
-}
-
-void display_array_stack() {
-    if (is_empty()) {
-        printf("Stack is empty.\n");
-        return;
-    }
-    printf("Stack (Top to Bottom): ");
-    for (int i = top; i >= 0; i--) {
-        printf("%d ", stack[i]);
-    }
-    printf("\n");
-}
-
-void reverse(){
-    
-}
+//main
+ void main()
+ {	printf("pushing: 10 20 30 40\n");
+ 	enque(10);
+ 	enque(20);
+ 	enque(30);
+ 	enque(40);
+ 	
+ 	printf("Original\n");
+ 	display();
+ 	
+ 	printf("Reverse\n");
+ 	reverse();
+ 	display();
+ }
